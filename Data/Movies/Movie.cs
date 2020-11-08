cc330f3e89a11ca14b37fcb35db4c6c036c5f335
Minh Phu Ngo <phu.ngo@outlook.com>
2020-11-08 18:25:03 -0500

    Tao danh sach va display movies

---
+++ Data/Movies/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data.Movies
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public int ReleaseDate { get; set; }
+        public string Genre { get; set; }
+    }
+}
